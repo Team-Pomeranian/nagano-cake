769ea59592df09e9801b951da1a51fc55c59d51b
[中村真] <abcmjqm@gmail.com>
2022-02-17 06:02:47 +0000

    [Update]カート内商品・配送先モデルのカラム追加

--- db/migrate/20220217052358_create_delivery_addresses.rb
+++ db/migrate/20220217052358_create_delivery_addresses.rb
@@ -2,10 +2,10 @@ class CreateDeliveryAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :delivery_addresses do |t|
 
-      t.integer "customer_id"
-      t.string "name"
-      t.string "postcode"
-      t.string "address"
+      t.integer "customer_id", null: false
+      t.string "name", null: false
+      t.string "postcode", null: false
+      t.string "address", null: false
 
       t.timestamps
     end
