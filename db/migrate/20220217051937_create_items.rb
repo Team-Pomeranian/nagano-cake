b869baad498d26b6d53ed5a3540cdd1c63a4596e
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-17 06:02:07 +0000

    [Fix]マイグレーションファイル

--- db/migrate/20220217051937_create_items.rb
+++ db/migrate/20220217051937_create_items.rb
@@ -1,6 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      
+      t.integer :genre_id, null: false
+      t.integer :name, null: false
+      t.text :introduction, null: false
+      t.integer :price_no_tax, null: false
+      t.string :item_image, null: false
+      t.boolean :sales_status, null: false, default: true
 
       t.timestamps
     end
