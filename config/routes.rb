1bd413965fd4b7582a7ccde56d89148352651853
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-18 04:15:07 +0000

    create-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,46 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'delivery_addresses/index'
+    get 'delivery_addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/complete'
+    get 'orders/confirm'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
