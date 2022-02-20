76d3c1fa962806ff32f127f1d27208848231c65a
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-20 04:29:08 +0000

    [add]unsubscribe

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,6 +8,13 @@ class Public::CustomersController < ApplicationController
   def unsubscribe
   end
 
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
   private
 
 
