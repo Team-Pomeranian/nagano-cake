169d5e5c7b9ad71a717e2e05ceb6da7a2f5a0830
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-20 14:40:42 +0000

    [Add]会員情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,8 +3,25 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customers).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :telephone_number)
+  end
+  
 end
