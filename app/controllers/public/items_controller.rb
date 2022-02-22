5ef6e5466cf34d08b025edffe021a59b74ba4335
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-22 07:08:26 +0000

    修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,7 @@ class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
     @items = Item.all.page(params[:page]).per(8)
+    @items_count = Item.count
   end
 
   def show
