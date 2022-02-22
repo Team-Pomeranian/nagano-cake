c0adc80c1190f0b9764155b45a350df8d26f89f9
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-22 08:21:07 +0000

    [Add]注文情報確認画面(未レイアウト）

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,6 +8,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_customer.cart_items.all
+    @order = Order.new(order_params)
+    @postage = 800
+    @total_fee = current_customer.cart_items.cart_items_total_price(@cart_items)
   end
 
   def index
@@ -18,4 +22,33 @@ class Public::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
+
+  def create
+    @cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    @total_fee = current_customer.cart_items.cart_items_total_price(@cart_items)
+    @postage = 800
+      if @order.save
+        @cart_items.each do |cart|
+          order_item = OrderItem.new(order_id: @order.id)
+          order_item.price = cart.item.price_no_tax
+          order_item.quantity = cart.quantity
+          order_item.item_id = cart.item_id
+          order_item.save!
+        end
+        redirect_to complete_orders_path
+        @cart_items.destroy_all
+      else
+        @order = Order.new(order_params)
+        render :new
+      end
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :name, :address, :postcode, :postage, :payment, :status, :total_price)
+  end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,11 @@ class CartItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
+  def self.cart_items_total_price(cart_items)
+    array = []
+    cart_items.each do |cart_item|
+      array << cart_item.item.price_no_tax * cart_item.quantity
+    end
+    return (array.sum * 1.1).floor
+  end
 end
