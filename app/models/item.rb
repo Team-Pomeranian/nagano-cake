bffb892e6d143b793ec9770ca2b1afe4d67bdae1
hisanori ito <86972518+hisanori-ito@users.noreply.github.com>
2022-02-23 13:32:23 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 
-  has_many :cart_items, dependent: :destoroy
+  has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genre
   has_one_attached :image
