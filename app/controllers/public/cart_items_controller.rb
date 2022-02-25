d1186d385c5c1fbe20d30abe67c12365187d98cb
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-25 03:46:26 +0000

    [add]flashメッセージの追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,7 +2,7 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = current_customer.cart_items
+    @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
@@ -12,27 +12,35 @@ class Public::CartItemsController < ApplicationController
             @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
             @cart_item.quantity += params[:cart_item][:quantity].to_i
             @cart_item.save
+            flash[:save] = "カートの商品を追加しました!"
+            redirect_to cart_items_path
+        elsif @cart_item.save
+            @cart_item.save
+            flash[:save] = "カートに商品が入りました！"
             redirect_to cart_items_path
         else
-            @cart_item.save
-            redirect_to cart_items_path
+            flash[:none] = "個数を選択してください"
+            redirect_to request.referer
         end
     end
 
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
+    flash[:update] = "商品の個数を変更しました。"
     redirect_to cart_items_path
   end
 
   def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    flash[:destroy] = "カートから商品を削除しました。"
     redirect_to cart_items_path
   end
 
   def destroy_all
     current_customer.cart_items.destroy_all
+    flash[:all] = "カートを空にしました。"
     redirect_to cart_items_path
   end
 
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-
+  validates :quantity, presence: true
 
   def self.cart_items_total_price(cart_items)
     array = []
