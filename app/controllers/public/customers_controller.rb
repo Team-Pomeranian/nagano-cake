21c2e9454d1200f08ea636565a8ee60b671f10e2
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-25 04:53:07 +0000

    [fix]carousel

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,6 +3,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
@@ -17,7 +27,8 @@ class Public::CustomersController < ApplicationController
 
   private
 
-
-
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :telephone_number)
+  end
 
 end
